9ffe7cf88d5891ba38a26a61afa3b806d0898084
Harrison Xu <hsx925@gmail.com>
2026-07-19 01:49:29 -0700

    Add: two-integer-sum - submission-8

---
+++ Data Structures & Algorithms/two-integer-sum/submission-8.py
@@ -0,0 +1,9 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        value_to_index = {}
+        for i, n in enumerate(nums):
+            diff = target - n
+            if diff in value_to_index:
+                return [value_to_index[diff], i]
+            value_to_index[n] = i
+        return []
